6e273c2dacfdae27e26cb663b0a5f946d2347639
prathjan <prathjan@cisco.com>
2023-10-11 15:27:37 -0700

    removed sudo

--- applications/Load-Generator/phantomjs/stop_load.sh
+++ applications/Load-Generator/phantomjs/stop_load.sh
@@ -15,19 +15,19 @@ sleep 1s
 ps -ef | grep appd-sc-mem-leak-insurance
 sleep 1s
 echo "Stopping processes for load scripts..."
-sudo pkill -f appd-sc-home-init
+pkill -f appd-sc-home-init
 sleep 1s
-sudo pkill -f appd-sc-slow-query
+pkill -f appd-sc-slow-query
 sleep 1s
-sudo pkill -f appd-sc-session
+pkill -f appd-sc-session
 sleep 1s
-sudo pkill -f appd-sc-sell-car
+pkill -f appd-sc-sell-car
 sleep 1s
-sudo pkill -f appd-sc-request-error
+pkill -f appd-sc-request-error
 sleep 1s
-sudo pkill -f appd-sc-search
+pkill -f appd-sc-search
 sleep 1s
-sudo pkill -f appd-sc-mem-leak-insurance
+pkill -f appd-sc-mem-leak-insurance
 sleep 1s
 echo "Checking for processes after stopping load..."
 ps -ef | grep appd-sc-home-init
